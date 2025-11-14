923caa0917901e04c21c6f7a46eac25753cce0e4
v-o-h-s <146709110+v-o-h-s@users.noreply.github.com>
2025-11-14 21:43:40 +0100

    feat: added new components for viewing user files

---
+++ components/pdf/Viewer.tsx
@@ -0,0 +1,8 @@
+export default function Viewer() {
+    return (
+        <div>
+            <h1>Viewer component is here</h1>
+            {/* PDF viewer implementation goes here */}
+        </div>
+    );
+}
