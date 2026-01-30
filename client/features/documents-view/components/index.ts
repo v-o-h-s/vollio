07f329cfe07f8414b8e445a06a40892ed3e6c997
v-o-h-s <146709110+v-o-h-s@users.noreply.github.com>
2026-01-30 15:12:03 +0100

    update the export

--- client/features/documents-view/components/index.ts
+++ client/features/documents-view/components/index.ts
@@ -1,6 +1,4 @@
 export { SearchBar } from "./SearchBar";
 export { ViewToggle } from "./ViewToggle";
-export { FilterDropdown } from "./FilterDropdown";
 export { DocumentsToolbar } from "./DocumentsToolbar";
 export type { ViewMode } from "./ViewToggle";
-export type { DocumentFilters } from "./FilterDropdown";
